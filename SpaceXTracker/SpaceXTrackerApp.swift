2f95004894ee495266cbaa8b92e39a5a45d694fe
DCHRKV <dchurakov97@gmail.com>
2023-01-10 20:17:42 +0000

    Initial Commit

---
+++ SpaceXTracker/SpaceXTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXTrackerApp.swift
+//  SpaceXTracker
+//
+//  Created by Dmytro Churakov on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
